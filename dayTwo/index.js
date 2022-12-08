bb4f20d197baec44f778dcb4dca06206ef5fc243
Alice Mirestes <amirestes@gmail.com>
2022-12-08 00:06:57 -0600

    day two challenge maping over a nested object

---
+++ dayTwo/index.js
@@ -0,0 +1,36 @@
+import userData from "./data.js";
+
+/* Totally Private Data Farm 
+
+Good news, renown advertising firm Evil Corp. wants to purchase our 
+private user data! 
+
+We'd never do this in real life of course, but just for practice 
+let's pretend we're unethical web hackers and transform the data 
+in the way Evil Corp. has requested. They're quite particular and
+just want an array of users with a fullname and human readable
+birthday.   
+
+Write a function that maps through the current data and returns
+a new an array of objects with only two properties: 
+fullName and birthday. Each result in your 
+array should look like this when you're done: 
+
+{
+    fullName: "Levent Busser", 
+    birthday: "Fri Aug 20 1971"
+}
+
+Read about toDateString() for info on formatting a readable date. 
+array.map(function(currentValue, index, arr), thisValue)
+*/
+function transformData(data){
+    return data.map(data => {
+        return{
+            fullName: `${data.name.first}  ${data.name.last}`,
+            birthday: new Date(data.dob.date).toDateString()
+        }
+    })
+}
+
+console.log(transformData(userData));
