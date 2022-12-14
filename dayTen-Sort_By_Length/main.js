04b16263c6ea86316dfc1ffb5b5854b67ff9699b
Alice Mirestes <amirestes@gmail.com>
2022-12-14 00:15:15 -0600

    completed day 10 sort by length

---
+++ dayTen-Sort_By_Length/main.js
@@ -0,0 +1,25 @@
+function sortByLength(strs) {
+    //  write code here.
+        return strs.sort((a,b) => a.length - b.length);
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('sortByLength()', () => {
+    it('sorts the strings from shortest to longest', () => {
+        // arrange
+        const strs = ["abc", "", "aaa", "a", "zz"];
+        
+        // act
+        const result = sortByLength(strs);
+
+        // log
+        console.log("result: ", result);
+        
+        // assert
+        expect(result).toEqual(["", "a", "zz", "abc", "aaa"]);
+    });
+});
