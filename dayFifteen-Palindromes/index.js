d672afd91fc01afd9e598bb8effb494f2c2c3ea3
Alice Mirestes <amirestes@gmail.com>
2022-12-19 10:37:02 -0600

    dayFifteen-Palindromes

---
+++ dayFifteen-Palindromes/index.js
@@ -0,0 +1,17 @@
+/* 
+Palindromes are words that are the same forward or backward. For example, 
+the words "noon" and "kayak" are a palindromes.
+ 
+Write a function to check if a lowercased string of letters is a palindrome. 
+If the word is palidrome, return true. If it isn't, return false. 
+*/
+
+function isPalindrome(str){
+   return str === str.split('').reverse().join('')
+}
+// Test your function
+console.log(isPalindrome("abba"));
+console.log(isPalindrome("civic"));
+console.log(isPalindrome("octopus"));
+console.log(isPalindrome("pumpkins"));
+console.log(isPalindrome("madam"));
