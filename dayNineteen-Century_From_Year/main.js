90a62fdcb932101bc9dd3378cbd1f264db8b3edb
Alice Mirestes <amirestes@gmail.com>
2022-12-19 19:12:08 -0600

    completed dayNineteen

---
+++ dayNineteen-Century_From_Year/main.js
@@ -0,0 +1,39 @@
+function centuryFromYear(num) {
+    //  write code here.
+    return Math.ceil(num / 100)
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('centuryFromYear()', () => {
+    it('returns current century', () => {
+        // arrange
+        const year = 1905;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 1: ", result);
+        
+        // assert
+        expect(result).toBe(20);
+    });
+    
+    it('returns current century for edge case of start of century', () => {
+        // arrange
+        const year = 1700;
+        
+        // act
+        const result = centuryFromYear(year);
+
+        // log
+        console.log("result 2: ", result);
+        
+        // assert
+        expect(result).toBe(17);
+    });    
+});
