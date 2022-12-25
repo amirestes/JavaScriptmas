a2a02dbbbbe6708ba06ee91ff469d2b512c794c6
Alice Mirestes <amirestes@gmail.com>
2022-12-25 09:31:39 -0600

    day 24 completed

--- dayTwenty-Four-Christmas_Jukebox/index.js
+++ dayTwenty-Four-Christmas_Jukebox/index.js
@@ -1,5 +1,6 @@
-const player = document.getElementById("player")
+const player = document.getElementById("player");
 
 function playSong(id) {
   // Challenge: Add code here to make the youtube player play the new YouTube song
+  player.src = `https://www.youtube.com/embed/${id}?autoplay=1`;
 }
