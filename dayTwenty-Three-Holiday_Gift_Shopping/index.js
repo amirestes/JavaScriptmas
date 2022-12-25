30a4d86f6d83ce6dbe6595d12234b11a4d8f3966
Alice Mirestes <amirestes@gmail.com>
2022-12-24 23:32:00 -0600

    day twenty three completed

---
+++ dayTwenty-Three-Holiday_Gift_Shopping/index.js
@@ -0,0 +1,24 @@
+import products from "./data.js";
+
+/*
+    You're online shopping for holiday gifts, but money is tight
+    so we need to look at the cheapest items first. 
+    Use the built in sort() method to write a function that returns a new array of
+    products sorted by price, cheapest to most expensive. 
+    
+    Then log the item and the price to the console: 
+    
+    💕,0
+    🍬,0.89
+    🍫,0.99
+    🧁,0.99
+    📚,0.99
+    ... continued
+*/
+
+function sortProducts(data) {
+  return data.sort((a, b) => a.price - b.price);
+}
+
+const listByCheapest = sortProducts(products);
+console.log(listByCheapest);
